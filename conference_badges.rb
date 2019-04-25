57f036acef25ab3aae2e7df5b68f160290b317f8
Maija Barrins <maija.barrins@gmail.com>
2019-04-25 21:00:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,8 @@ end
 def assign_rooms(names)
   names.each_with_index.map{|name, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
 end
+
+def printer(names)
+  batch_badge_creator(names).each{|badge| puts badge}
+  assign_rooms(names).each{|room| puts room}
+end
