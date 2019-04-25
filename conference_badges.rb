160556c6bc27e26f775d8cc8ed3111d3c583e62c
Maija Barrins <maija.barrins@gmail.com>
2019-04-25 20:57:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,4 +6,5 @@ def batch_badge_creator(names)
   names.map{|name| badge_maker(name)}
 end
 
-  
+def assign_rooms(names)
+  names.each_with_index.map{|name, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
